2ad7206a25af121d956f089925be20f0438e41c8
Henry Pham <henryph.it@gmail.com>
2019-06-05 00:12:49 +1200

    Henry Commits all existing file

--- app/src/main/java/it/hp/biot/Ethereum/SmartContract.java
+++ app/src/main/java/it/hp/biot/Ethereum/SmartContract.java
@@ -89,8 +89,6 @@ public class SmartContract {
 
     public void connectToEthNetwork() {
 
-        // FIXME: Add your own API key here
-
         web3j = Web3j.build(new HttpService("https://ropsten.infura.io/v3/47c63d5fab244e4ba2311dd0a9ce35eb"));
         try {
             Web3ClientVersion clientVersion = web3j.web3ClientVersion().sendAsync().get();
@@ -110,25 +108,6 @@ public class SmartContract {
 
         }
     }
-
-    private void setupBouncyCastle() {
-        final Provider provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME);
-        if (provider == null) {
-            // Web3j will set up the provider lazily when it's first used.
-            return;
-        }
-        if (provider.getClass().equals(BouncyCastleProvider.class)) {
-            // BC with same package name, shouldn't happen in real life.
-            return;
-        }
-        // Android registers its own BC provider. As it might be outdated and might not include
-        // all needed ciphers, we substitute it with a known BC bundled in the app.
-        // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
-        // of that it's possible to have another BC implementation loaded in VM.
-        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
-        Security.insertProviderAt(new BouncyCastleProvider(), 1);
-    }
-
 
     public void createWallet() {
         try {
